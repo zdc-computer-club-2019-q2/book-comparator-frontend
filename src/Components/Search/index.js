5b6aa62b3b7db3297429bff276463251e1022b3b
DNZio <45425290+DNZio@users.noreply.github.com>
2019-07-26 17:44:46 +0800

    Add Search style

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -1,28 +1,36 @@
-import React, {useState} from 'react';
-import './search.css';
+import React, { useState } from "react";
+import "./search.css";
+import logo from "../../images/simplye2.png";
 
 function Search() {
-    const [key, setKey] = useState(""); // key string for search, using https://reactjs.org/docs/hooks-intro.html
+  const [key, setKey] = useState(""); // key string for search, using https://reactjs.org/docs/hooks-intro.html
 
-    function onClickSearchBtn(e) {
-        e.preventDefault();
-        // TODO: What to do when the user clicked Search button?
-        console.log(key);
-    }
+  function onClickSearchBtn(e) {
+    e.preventDefault();
+    // TODO: What to do when the user clicked Search button?
+    console.log(key);
+  }
 
-    return (
-        <div id="search">
-            <input
-                className="input"
-                onChange={e => setKey(e.target.value)} placeholder="Search your book here" value={key}/>
-            <button
-                className="button"
-                onClick={onClickSearchBtn}>Search!</button>
-            <p className="debug">
-                For debug: the key you input is: {key}
-            </p>
-        </div>
-    );
+  return (
+    <div id="search">
+      <div>
+        <img src={logo} />
+      </div>
+      <div id="searchfield">
+        <input
+          align="middle"
+          className="input"
+          onChange={e => setKey(e.target.value)}
+          placeholder="Search..."
+          value={key}
+        />
+        <button className="button" onClick={onClickSearchBtn}>
+          Search!
+        </button>
+      </div>
+      <p className="debug">For debug: the key you input is: {key}</p>
+    </div>
+  );
 }
 
 export default Search;
