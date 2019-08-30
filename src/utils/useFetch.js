4169f1287612e87fc0b3c76fc53bddf3d3c10cf3
Sutris <sputra@zendesk.com>
2019-08-30 19:00:06 +0800

    Add useFetch

---
+++ src/utils/useFetch.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const useFetch = (url, options) => {
+  const [response, setResponse] = React.useState(null);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url, options);
+        const json = await res.json();
+        setResponse(json);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchData();
+  }, [options, url]);
+
+  return { response, error };
+};
+
+export default useFetch;
