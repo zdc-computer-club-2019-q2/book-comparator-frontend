1e3a2a1bf7a11d416a19d0fc640bcbe74b8e2649
Hee Jun Yi <junyi.hjy@gmail.com>
2019-08-30 19:16:17 +0800

    Fix config

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
-export const GOOGLE_BOOKS_API_KEY = "AIzaSyDQmBf3h-1q520IJjR0m5jI1DitSzaXxHc";
-export const NYT_API_KEY = "6ad84e249d054efeaefe1abb8f89df5b";
+module.exports = {
+    GOOGLE_BOOKS_API_KEY: "AIzaSyDQmBf3h-1q520IJjR0m5jI1DitSzaXxHc",
+    NYT_API_KEY: "6ad84e249d054efeaefe1abb8f89df5b"
+};
