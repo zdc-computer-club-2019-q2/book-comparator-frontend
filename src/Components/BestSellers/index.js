66d305dd390abe3a39e3c48bab31e33025d2b626
Hee Jun Yi <junyi.hjy@gmail.com>
2019-08-30 19:20:56 +0800

    Connect bestseller with backend

--- src/Components/BestSellers/index.js
+++ src/Components/BestSellers/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import useFetch from "../../utils/useFetch";
 
 import "./bestSellers.css";
 
@@ -8,115 +9,82 @@ import fbs_3 from "../../images/fbs_3.jpg";
 import nfbs_1 from "../../images/nfbs_1.jpg";
 
 function Header({ type }) {
-  return (
-    <h1 className="header">
-      {type} Best Sellers
-      <span class="icon is-small is-left">
-        <i class="fas fa-chevron-right"></i>
-      </span>
-    </h1>
-  );
+    return (
+        <h1 className="header">
+            {type} Best Sellers
+            <span className="icon is-small is-left">
+                <i className="fas fa-chevron-right"></i>
+            </span>
+        </h1>
+    );
 }
 
-function BestSeller({ author, imageUrl, tag, title, isbn}) {
-  // TODO: link to Book
-  const url = "/book/" + isbn;
-  return (
-    <a href={url}>
-      <div className="card">
-        <div
-            className="card__image"
-            style={{ backgroundImage: `url(${imageUrl})` }}
-        />
-        <div className="card__copy">
-          <div className="media">
-            <div className="media-content">
-              <span className="book-tag card__tag">{tag ? tag.toUpperCase() : "TAG"}</span>
-              <div className="card__title">{title.toUpperCase()}</div>
-              <div className="card__author">by <span>{author}</span></div>
+function BestSeller({ author, imageUrl, tag, title, isbn }) {
+    // TODO: link to Book
+    const url = "/book/" + isbn;
+    return (
+        <a href={url} className="card__container">
+            <div className="card">
+                <div className="card__image" style={{ backgroundImage: `url(${imageUrl})` }} />
+                <div className="card__copy">
+                    <div className="media">
+                        <div className="media-content">
+                            <span className="book-tag card__tag">{tag ? tag.toUpperCase() : "TAG"}</span>
+                            <div className="card__title">{title.toUpperCase()}</div>
+                            <div className="card__author">
+                                by <span>{author}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-     </a>
-  );
+        </a>
+    );
 }
 
 function BestSellers() {
-  const domain = "https://book-comparator-frontend-git-mockups.zdc-computer-club-2019-q2.now.sh";
-  let url_fiction = fetch("/api/bestseller?type=fiction", []);
-  let url_nonfiction = fetch("/api/bestseller?type=nonfiction", []);
-  fetch(domain + "/api/search?q=abc", { mode: 'cors' })
-    .then(function(response) {
-      response.json().then(function(data) {
-        console.log(data);
-      });
-    })
-    .catch(function(error) {
+    let { response: fiction } = useFetch("/api/bestseller?type=fiction");
+    let { response: nonfiction } = useFetch("/api/bestseller?type=nonfiction");
 
-    })
+    return (
+        <div id="bestsellers">
+            <div className="bestsellers__section">
+                <Header type="Fiction" />
+                <div class="bestsellers">
+                    {fiction &&
+                        fiction.results
+                            .slice(0, 6)
+                            .map(({ isbn, image, author, title, bestseller_weeks }) => (
+                                <BestSeller
+                                    author={author}
+                                    imageUrl={image}
+                                    title={title}
+                                    isbn={isbn}
+                                    tag={`${bestseller_weeks} weeks`}
+                                />
+                            ))}
+                </div>
+            </div>
 
-  return (
-    <div id="bestsellers">
-      <div className="bestsellers__section">
-        <Header type="Fiction" />
-        <div class="bestsellers">
-          <BestSeller
-            author="Agatha Christie"
-            imageUrl={fbs_1}
-            title="The ABC Murders"
-            isbn="1234"
-          />
-          <BestSeller author="Cecelia Ahern" imageUrl={fbs_2} title="The Gift" />
-          <BestSeller
-            author="Antoine de Saint-Exupéry"
-            imageUrl={fbs_3}
-            title="The Little Prince"
-            isbn="1234"
-          />
-          <BestSeller
-            author="Amy Newmark"
-            imageUrl={nfbs_1}
-            title="Chicken Soup for the Soul"
-            isbn="1234"
-          />
-          <BestSeller
-              author="Agatha Christie"
-              imageUrl={fbs_1}
-              title="The ABC Murders"
-              isbn="1234"
-          />
+            <div className="bestsellers__section">
+                <Header type="Non Fiction" />
+                <div className="bestsellers">
+                    {nonfiction &&
+                        nonfiction.results
+                            .slice(0, 6)
+                            .map(({ isbn, image, author, title, bestseller_weeks }) => (
+                                <BestSeller
+                                    author={author}
+                                    imageUrl={image}
+                                    title={title}
+                                    isbn={isbn}
+                                    tag={`${bestseller_weeks} weeks`}
+                                />
+                            ))}
+                </div>
+            </div>
         </div>
-      </div>
-      
-      <div className="bestsellers__section">
-        <Header type="Non Fiction"/>
-        <div className="bestsellers">
-          <BestSeller
-              author="Agatha Christie"
-              imageUrl={fbs_1}
-              title="The ABC Murders"
-          />
-          <BestSeller author="Cecelia Ahern" imageUrl={fbs_2} title="The Gift"/>
-          <BestSeller
-              author="Antoine de Saint-Exupéry"
-              imageUrl={fbs_3}
-              title="The Little Prince"
-          />
-          <BestSeller
-              author="Amy Newmark"
-              imageUrl={nfbs_1}
-              title="Chicken Soup for the Soul"
-          />
-          <BestSeller
-              author="Agatha Christie"
-              imageUrl={fbs_1}
-              title="The ABC Murders"
-          />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default BestSellers;
