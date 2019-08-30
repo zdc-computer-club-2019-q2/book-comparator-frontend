f25983804068f9709e01a165f187e16049485f22
Hee Jun Yi <junyi.hjy@gmail.com>
2019-08-30 19:36:12 +0800

    Add image API

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var cors = require("cors");
 var searchRouter = require("./api/search");
 var bookRouter = require("./api/book");
 var bestsellerRouter = require("./api/bestseller");
+var imageRouter = require("./api/image");
 
 var app = express();
 app.use(cors());
@@ -11,6 +12,7 @@ app.use(cors());
 app.use("/api/search", searchRouter);
 app.use("/api/book", bookRouter);
 app.use("/api/bestseller", bestsellerRouter);
+app.use("/api/image", imageRouter);
 
 app.listen(5000);
 
