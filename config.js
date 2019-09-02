1ded2846fbc758a011dc9002ccbee65fd901a1d5
Hee Jun Yi <junyi.hjy@gmail.com>
2019-09-02 23:59:49 +0800

    Rotate Google Books API key

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    GOOGLE_BOOKS_API_KEY: "AIzaSyDQmBf3h-1q520IJjR0m5jI1DitSzaXxHc",
+    GOOGLE_BOOKS_API_KEY: "AIzaSyBbdwHjDlG7cGhP7BtTiyEpFokZY6c4P6s",
     NYT_API_KEY: "6ad84e249d054efeaefe1abb8f89df5b"
 };
