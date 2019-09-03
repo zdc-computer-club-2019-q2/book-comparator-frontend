02df772288c9c399bf024691504a682b6ec0b621
Hee Jun Yi <junyi.hjy@gmail.com>
2019-09-03 16:11:06 +0800

    Remove console.log

--- api/search.js
+++ api/search.js
@@ -17,7 +17,6 @@ function search_result(volumeInfo) {
 
 /* Search by keyword. */
 module.exports = function(req, res) {
-    console.log("GOOGLE_BOOKS_API", GOOGLE_BOOKS_API);
     request.get(`${GOOGLE_BOOKS_API}&q=${req.query.q}`, (err, result) => {
         var sellable_items = (JSON.parse(result.body).items || [])
             .filter(function(item) {
