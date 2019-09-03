74f96aeece758fe066c94797465a9dbc80ac6454
Hee Jun Yi <junyi.hjy@gmail.com>
2019-09-03 09:07:49 +0800

    Fix search result page

--- src/Components/Home/SearchResult/index.js
+++ src/Components/Home/SearchResult/index.js
@@ -1,83 +1,74 @@
-import React from 'react';
+import React from "react";
 
-import useFetch from '../../../utils/useFetch';
+import useFetch from "../../../utils/useFetch";
 
-import Loader from '../../Loader';
+import Loader from "../../Loader";
 
-import './searchResult.css';
+import "./searchResult.css";
 
 function Result({ author, title, onClick, isbn, bestSeller = false }) {
-  return (
-    <div className="search-result" onClick={onClick}>
-      <div
-        className="search-result__image"
-        style={{
-          backgroundImage: `url(/api/image?isbn=${isbn})`
-        }}
-      />
-      <div className="search-result__copy">
-        <div className="search-result__title">{title.toUpperCase()}</div>
-        <div className="search-result__author">
-          by <span>{author}</span>
+    return (
+        <div className="search-result" onClick={onClick}>
+            <div
+                className="search-result__image"
+                style={{
+                    backgroundImage: `url(/api/image?isbn=${isbn})`
+                }}
+            />
+            <div className="search-result__copy">
+                <div className="search-result__title">{title.toUpperCase()}</div>
+                <div className="search-result__author">
+                    by <span>{author}</span>
+                </div>
+                {bestSeller && <div className="book-tag search-result__tag book-tag--bestseller">Best Seller</div>}
+            </div>
         </div>
-        {bestSeller && (
-          <div className="book-tag search-result__tag book-tag--bestseller">
-            Best Seller
-          </div>
-        )}
-      </div>
-    </div>
-  );
+    );
 }
 
 function SearchResult({ location, history }) {
-  const urlParams = new URLSearchParams(location.search);
-  const searchTerm = urlParams.get("q");
+    const urlParams = new URLSearchParams(location.search);
+    const searchTerm = urlParams.get("q");
 
-  const { response, error } = useFetch(`/api/search?q=${searchTerm}`);
+    const { response, error } = useFetch(`/api/search?q=${searchTerm}`);
 
-  function onClickSearchResult(isbn) {
-    history.push(`/book/${isbn}`);
-  }
+    function onClickSearchResult(isbn) {
+        history.push(`/book/${isbn}`);
+    }
 
-  if (error !== null) {
-    window.alert(error);
-    return
-  }
+    if (error !== null) {
+        window.alert(error);
+        return;
+    }
 
-  const { results, results_count } = response;
+    if (!response) {
+        return <Loader />;
+    }
 
-  return (
-    <div className="search-results">
-      {
-        response === null ?
-          <Loader /> :
-          (
-            <React.Fragment>
-              <div className="search-results__number">
-                Showing <b>{results_count}</b> result{results_count > 1 ? "s" : ""}.
-              </div>
-              {
-                results.map((result, i) => (
-                  <div
-                    className="search-results__result"
-                    key={`search-result-${i}`}
-                  >
-                    <Result
-                      {...result}
-                      onClick={() => {
-                        onClickSearchResult(result.isbn);
-                      }}
-                      bestSeller
-                    />
-                  </div>
-                ))
-              }
-            </React.Fragment>
-          )
-      }
-    </div>
-  );
+    const { results, results_count } = response;
+
+    return (
+        <div className="search-results">
+            {
+                <React.Fragment>
+                    <div className="search-results__number">
+                        Showing <b>{results_count}</b> result{results_count > 1 ? "s" : ""}.
+                    </div>
+                    {results.map((result, i) => (
+                        <div className="search-results__result" key={`search-result-${i}`}>
+                            <Result
+                                {...result}
+                                onClick={() => {
+                                    onClickSearchResult(result.isbn);
+                                }}
+                                bestSeller
+                            />
+                        </div>
+                    ))}
+                </React.Fragment>
+            }
+        </div>
+    );
 }
 
 export default SearchResult;
