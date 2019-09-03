43e38e8528108926501b86eada3423852e814c63
Hee Jun Yi <junyi.hjy@gmail.com>
2019-09-03 14:19:08 +0800

    Use keys from env variable

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    GOOGLE_BOOKS_API_KEY: "AIzaSyBbdwHjDlG7cGhP7BtTiyEpFokZY6c4P6s",
-    NYT_API_KEY: "6ad84e249d054efeaefe1abb8f89df5b"
+    GOOGLE_BOOKS_API_KEY: process.env.GOOGLE_BOOKS_API_KEY,
+    NYT_API_KEY: process.env.NYT_API_KEY
 };
